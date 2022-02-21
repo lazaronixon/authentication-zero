a3f63809b413e61b8ed493577e272b80e4c79ac4
Nixon <lazaronixon@hotmail.com>
2022-02-21 13:25:17 -0300

    add more generator options

--- lib/generators/authentication/authentication_generator.rb
+++ lib/generators/authentication/authentication_generator.rb
@@ -2,7 +2,15 @@ require "rails/generators/active_record"
 
 class AuthenticationGenerator < Rails::Generators::NamedBase
   class_option :api, type: :boolean, desc: "Generates API authentication"
+
+  class_option :migration, type: :boolean
+  class_option :test_framework, type: :string, desc: "Test framework to be invoked"
+
+  class_option :fixture, type: :boolean
   class_option :system_tests, type: :string, desc: "Skip system test files"
+
+  class_option :skip_routes, type: :boolean
+  class_option :template_engine, type: :string, desc: "Template engine to be invoked"
 
   source_root File.expand_path("templates", __dir__)
 
@@ -10,17 +18,23 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
     uncomment_lines "Gemfile", /bcrypt/
   end
 
-  def create_migrations
-    invoke "migration", ["create_#{table_name}", "email:string:uniq", "password_digest:string", "session_token:string:uniq"]
+  def create_migration
+    if options.migration
+      invoke "migration", ["create_#{table_name}", "email:string:uniq", "password:digest", "session_token:string:uniq"]
+    end
   end
 
-  def create_models
-    template "app/models/model.rb", "app/models/#{file_name}.rb"
-    template "app/models/current.rb", "app/models/current.rb"
+  def create_model
+    template "models/model.rb", "app/models/#{file_name}.rb"
+    template "models/current.rb", "app/models/current.rb"
   end
+
+  hook_for :fixture_replacement
 
   def create_fixture_file
-    template "test_unit/fixtures.yml", "test/fixtures/#{fixture_file_name}.yml"
+    if options.fixture
+      template "#{test_framework}/fixtures.yml", "test/fixtures/#{fixture_file_name}.yml"
+    end
   end
 
   def add_application_controller_methods
@@ -55,38 +69,40 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
   end
 
   def create_controllers
-    directory "app/controllers/#{format_folder}", "app/controllers"
+    directory "controllers/#{format_folder}", "app/controllers"
   end
 
   def create_views
     if options.api
-      directory "app/views/email_mailer", "app/views/email_mailer"
-      directory "app/views/password_mailer", "app/views/password_mailer"
+      directory "#{template_engine}/email_mailer", "app/views/email_mailer"
+      directory "#{template_engine}/password_mailer", "app/views/password_mailer"
     else
-      directory "app/views", "app/views"
+      directory "#{template_engine}", "app/views"
     end
   end
 
   def create_mailers
-    template "app/mailers/email_mailer.rb", "app/mailers/email_mailer.rb"
-    template "app/mailers/password_mailer.rb", "app/mailers/password_mailer.rb"
+    template "mailers/email_mailer.rb", "app/mailers/email_mailer.rb"
+    template "mailers/password_mailer.rb", "app/mailers/password_mailer.rb"
   end
 
   def add_routes
-    route "resource :password_resets, only: [:new, :edit, :create, :update]"
-    route "resource :cancellations, only: [:new, :create]"
-    route "resource :passwords, only: [:edit, :update]"
-    route "resource :emails, only: [:edit, :update]"
-    route "delete 'sign_out', to: 'sessions#destroy'"
-    route "post 'sign_up', to: 'registrations#create'"
-    route "get 'sign_up', to: 'registrations#new'" unless options.api?
-    route "post 'sign_in', to: 'sessions#create'"
-    route "get 'sign_in', to: 'sessions#new'" unless options.api?
+    unless options.skip_routes
+      route "resource :password_resets, only: [:new, :edit, :create, :update]"
+      route "resource :cancellations, only: [:new, :create]"
+      route "resource :passwords, only: [:edit, :update]"
+      route "resource :emails, only: [:edit, :update]"
+      route "delete 'sign_out', to: 'sessions#destroy'"
+      route "post 'sign_up', to: 'registrations#create'"
+      route "get 'sign_up', to: 'registrations#new'" unless options.api?
+      route "post 'sign_in', to: 'sessions#create'"
+      route "get 'sign_in', to: 'sessions#new'" unless options.api?
+    end
   end
 
   def create_test_files
-    directory "test_unit/controllers/#{format_folder}", "test/controllers"
-    directory "test_unit/system", "test/system" if system_tests?
+    directory "#{test_framework}/controllers/#{format_folder}", "test/controllers"
+    directory "#{system_tests}/system", "test/system" if system_tests?
   end
 
   private
@@ -94,7 +110,19 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
       options.api ? "api" : "html"
     end
 
+    def template_engine
+      options.template_engine
+    end
+
+    def test_framework
+      options.test_framework
+    end
+
+    def system_tests
+      options.system_tests
+    end
+
     def system_tests?
-      !options.api? && options[:system_tests]
+      !options.api? && options.system_tests
     end
 end
