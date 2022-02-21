dec4d019033fb61dbb93da23850c1ef6d194c64c
Nixon <lazaronixon@hotmail.com>
2022-02-21 03:07:52 -0300

    remove code injection into helper methods

--- lib/generators/authentication/authentication_generator.rb
+++ lib/generators/authentication/authentication_generator.rb
@@ -21,34 +21,8 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
     template "app/models/current.rb", "app/models/current.rb"
   end
 
-  def create_controllers
-    directory "app/controllers/#{format_folder}", "app/controllers"
-  end
-
-  def create_views
-    if options.api
-      directory "app/views/email_mailer", "app/views/email_mailer"
-      directory "app/views/password_mailer", "app/views/password_mailer"
-    else
-      directory "app/views", "app/views"
-    end
-  end
-
-  def create_mailers
-    template "app/mailers/email_mailer.rb", "app/mailers/email_mailer.rb"
-    template "app/mailers/password_mailer.rb", "app/mailers/password_mailer.rb"
-  end
-
-  def add_routes
-    route "resource :password_resets, only: [:new, :edit, :create, :update]"
-    route "resource :cancellations, only: [:new, :create]"
-    route "resource :passwords, only: [:edit, :update]"
-    route "resource :emails, only: [:edit, :update]"
-    route "delete 'sign_out', to: 'sessions#destroy'"
-    route "post 'sign_up', to: 'registrations#create'"
-    route "get 'sign_up', to: 'registrations#new'" unless options.api?
-    route "post 'sign_in', to: 'sessions#create'"
-    route "get 'sign_in', to: 'sessions#new'" unless options.api?
+  def create_fixture_file
+    template "test_unit/fixtures.yml", "test/fixtures/#{fixture_file_name}.yml"
   end
 
   def add_application_controller_methods
@@ -78,51 +52,38 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
         end
     CODE
 
-    source = "app/controllers/application_controller.rb"
-    klass  = "ApplicationController"
-    injection_code = options.api? ? api_code : html_code
-    inject_into_class source, klass, optimize_indentation(injection_code, 2), verbose: false
+    inject_code = options.api? ? api_code : html_code
+    inject_into_class "app/controllers/application_controller.rb", "ApplicationController", optimize_indentation(inject_code, 2), verbose: false
   end
 
-  def create_fixture_file
-    template "test_unit/fixtures.yml", "test/fixtures/#{fixture_file_name}.yml"
+  def create_controllers
+    directory "app/controllers/#{format_folder}", "app/controllers"
   end
 
-  def add_test_helpers_methods
-    api_code = <<~CODE
-      def sign_in_as(#{singular_table_name})
-        post(sign_in_url, params: { email: #{singular_table_name}.email, password: "secret123" })
-        [#{singular_table_name}, response.parsed_body["session_token"]]
-      end
-    CODE
-
-    html_code = <<~CODE
-      def sign_in_as(#{singular_table_name})
-        post(sign_in_url, params: { email: #{singular_table_name}.email, password: "secret123" }); #{singular_table_name}
-      end
-    CODE
-
-    injection_code = options.api? ? api_code : html_code
-    inject_into_file "test/test_helper.rb", optimize_indentation(injection_code, 2), before: /^end/, verbose: false
-  end
-
-  def add_system_test_helpers_methods
-    if !options.api? && options[:system_tests]
-      injection_code = <<~CODE
-
-        def sign_in_as(user)
-          visit sign_in_url
-          fill_in :email, with: user.email
-          fill_in :password, with: "secret123"
-          click_on "Sign in"
-
-          user
-        end
-      CODE
-
-      source = "test/application_system_test_case.rb"
-      inject_into_file source, optimize_indentation(injection_code, 2), before: /^end/, verbose: false
+  def create_views
+    if options.api
+      directory "app/views/email_mailer", "app/views/email_mailer"
+      directory "app/views/password_mailer", "app/views/password_mailer"
+    else
+      directory "app/views", "app/views"
     end
+  end
+
+  def create_mailers
+    template "app/mailers/email_mailer.rb", "app/mailers/email_mailer.rb"
+    template "app/mailers/password_mailer.rb", "app/mailers/password_mailer.rb"
+  end
+
+  def add_routes
+    route "resource :password_resets, only: [:new, :edit, :create, :update]"
+    route "resource :cancellations, only: [:new, :create]"
+    route "resource :passwords, only: [:edit, :update]"
+    route "resource :emails, only: [:edit, :update]"
+    route "delete 'sign_out', to: 'sessions#destroy'"
+    route "post 'sign_up', to: 'registrations#create'"
+    route "get 'sign_up', to: 'registrations#new'" unless options.api?
+    route "post 'sign_in', to: 'sessions#create'"
+    route "get 'sign_in', to: 'sessions#new'" unless options.api?
   end
 
   def create_test_files
