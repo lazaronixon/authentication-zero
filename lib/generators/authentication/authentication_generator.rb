83a10a8c2932e4a9446d42ba9822cc58cd29fa3a
Nixon <lazaronixon@hotmail.com>
2022-02-24 18:26:47 -0300

    reactor api - change email_verification#update to email_verification#edit in order to match the html template

--- lib/generators/authentication/authentication_generator.rb
+++ lib/generators/authentication/authentication_generator.rb
@@ -96,7 +96,7 @@ class AuthenticationGenerator < Rails::Generators::NamedBase
       route "resource :registration, only: :destroy"
       route "resource :password_reset, only: [:new, :edit, :create, :update]"
       route "resource :password, only: [:edit, :update]"
-      route "resource :email_verification, only: [:new, :edit, :create, :update]"
+      route "resource :email_verification, only: [:edit, :create]"
       route "resource :email, only: [:edit, :update]"
       route "resources :sessions, only: [:index, :show, :destroy]"
       route "post 'sign_up', to: 'registrations#create'"
