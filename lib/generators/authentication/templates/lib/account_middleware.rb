ec6464114f85e010696de9eada2e7d750a1165eb
Nixon <lazaronixon@hotmail.com>
2023-06-02 02:33:04 -0300

    Don't raise record not found on account middleware

--- lib/generators/authentication/templates/lib/account_middleware.rb
+++ lib/generators/authentication/templates/lib/account_middleware.rb
@@ -8,9 +8,9 @@ class AccountMiddleware
 
     _, account_id, request_path = request.path.split("/", 3)
 
-    if is_number?(account_id)
+    if identifier?(account_id)
       set_current_account(account_id)
-      
+
       request.script_name = "/#{account_id}"
       request.path_info   = "/#{request_path}"
       @app.call(request.env)
@@ -20,11 +20,11 @@ class AccountMiddleware
   end
 
   private
-    def is_number?(value)
-      Integer(value, exception: false)
+    def identifier?(value)
+      Integer(value, exception: false) != nil
     end
 
     def set_current_account(account_id)
-      Current.account = Account.find(account_id)
+      Current.account = Account.find_by_id(account_id)
     end
 end
