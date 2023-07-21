be492d68575b8ab2c18336b1e44ef1e684a7e1b9
Nixon <lazaronixon@hotmail.com>
2023-07-21 02:48:16 -0300

    AccountMiddleware refactor

--- lib/generators/authentication/templates/lib/account_middleware.rb
+++ lib/generators/authentication/templates/lib/account_middleware.rb
@@ -6,13 +6,11 @@ class AccountMiddleware
   def call(env)
     request = ActionDispatch::Request.new(env)
 
-    _, account_id, request_path = request.path.split("/", 3)
-
-    if account_id !~ /\D/
+    if m = /\A(\/(\d{1,}))/.match(request.path_info)
+      script_name, account_id, path_info = [m[1], m[2], m.post_match]
+      request.script_name = script_name
+      request.path_info   = path_info.presence || "/"
       set_current_account(account_id)
-
-      request.script_name = "/#{account_id}"
-      request.path_info   = "/#{request_path}"
       @app.call(request.env)
     else
       @app.call(request.env)
